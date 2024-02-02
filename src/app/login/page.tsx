a82ff852e6232e0d356ea809a5a8d8ad78ca85ba
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-02 22:22:39 +0800

    updated deploy

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex h-screen w-screen justify-center place-items-center">
-      <h1>This is the login page</h1>
+      <h1>Yo, This is the login page</h1>
     </main>
   );
 }
