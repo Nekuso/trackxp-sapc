f3646504ab2cdf8f7068e1dbb5afce158ebab4cd
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-02 22:58:24 +0800

    reset

--- src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex h-screen w-screen justify-center place-items-center">
-      <h1>Yo, This is the login page</h1>
-    </main>
-  );
-}
--- src/app/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex h-screen w-screen justify-center place-items-center">
-      <h1>This is the sign page</h1>
-    </main>
-  );
-}
