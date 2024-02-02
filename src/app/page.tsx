ae12e79617ff7a6590525a36c1a16cbb61f08a26
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-03 02:09:39 +0800

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center place-items-center">
-      This is the Home Page
+      LANDING PAGE UNDER CONSTRUCTION
     </div>
   );
 }
