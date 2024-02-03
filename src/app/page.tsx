e3c843a781d9b7418185914aa710b07558a34a45
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-03 09:43:23 +0800

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center place-items-center">
-      LANDING PAGE UNDER CONSTRUCTION v2
+      LANDING PAGE UNDER CONSTRUCTION v3
     </div>
   );
 }
