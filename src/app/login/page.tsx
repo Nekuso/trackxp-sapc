1fbd8d9d85f63e2ffc10f31c19a189980b69e10e
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-03 12:07:45 +0800

    Updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center place-items-center">
-      This is the Login Page
+      This is the Login Page v2
     </div>
   );
 }
