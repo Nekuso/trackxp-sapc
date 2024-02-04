d728fb884f67357408745a040c5d1f41a70f0d03
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-04 13:09:43 +0800

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-center place-items-center">
-      <div className="p-4 border border-slate-600 bg-slate-800 rounded-lg shadow-xl">
+      <div className="p-4 border border-slate-600 bg-slate-800 rounded-md shadow-xl">
         This landing page is under construction
       </div>
     </div>
