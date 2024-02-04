bc7695a950876bbf40d702b9a69df241e5f28fe8
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-04 11:34:32 +0800

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center place-items-center">
-      LANDING PAGE UNDER CONSTRUCTION v3
+    <div className="w-full h-screen flex flex-col justify-center place-items-center">
+      <div className="p-4 border border-slate-600 bg-slate-800 rounded-lg shadow-xl">
+        This landing page is under construction
+      </div>
     </div>
   );
 }
