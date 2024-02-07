c2b7d6c5f99613a892a3d979eb741395c85396d8
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-02-07 11:45:09 +0800

    Added Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,32 +4,32 @@ import Logo from "@/images/sapsc-logo-light.png";
 
 export default function Nav() {
   return (
-    <nav className="w-full h-auto py-4 flex justify-between place-items-center">
-      <div className="w-[12%]">
+    <nav className="w-full h-auto py-6 flex justify-between place-items-center">
+      <div className="w-[13%]">
         <Image src={Logo} alt="SAPSC Logo" className="w-full"></Image>
       </div>
-      <div className="w-fit flex justify-center place-items-center gap-32">
+      <div className="w-fit flex justify-center place-items-center gap-16">
         <ul className="w-full flex gap-8">
-          <Link href="/" className="text-sm">
+          <Link href="/" className="text-xs font-medium">
             Home
           </Link>
-          <Link href="/" className="text-sm">
+          <Link href="/" className="text-xs font-medium">
             Services
           </Link>
-          <Link href="/" className="text-sm">
+          <Link href="/" className="text-xs font-medium">
             Produts
           </Link>
-          <Link href="/" className="text-sm">
+          <Link href="/" className="text-xs font-medium">
             FAQ
           </Link>
-          <Link href="/" className="text-sm">
+          <Link href="/" className="text-xs font-medium">
             About
           </Link>
         </ul>
 
         <Link
           href="/"
-          className="w-fit h-fit px-6 py-3 bg-white text-black rounded-full font-bold text-sm hover: hover:bg-slate-200 transition duration-300 shadow-lg"
+          className="w-fit h-fit px-6 py-3 bg-white text-black rounded-full font-bold text-xs hover: hover:bg-slate-200 transition duration-300 shadow-lg"
         >
           Login
         </Link>
